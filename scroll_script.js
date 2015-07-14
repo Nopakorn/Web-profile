73b3c630d0f60b315566498407cef098058770eb
Nopakorn <nopakorn.g@ssd.co.th>
2015-07-14 13:56:40 +0700

    add animation
    add animation and script for scrolling controller

--- scroll_script.js
+++ scroll_script.js
@@ -1,18 +1,27 @@
 
 $(function(){
-    var shrinkHeader = 50;
+    var shrinkHeader = 14;
+
+    $('.about-me').hide();
+    $('.work-ex').hide();
+
+    $('.intro-self').addClass('animated fadeInUp');
+    $('.social-ic').addClass('animated fadeInUp');
+
+
 
     $(window).scroll(function() {
         var scroll = getCurrentScroll();
         console.log(scroll);
         if ( scroll >= shrinkHeader ) {
-            $('.page-header').addClass('slideClosed');
-
+            $('.about-me').show();
+            $('.about-me').addClass('animated fadeInUp');
+            $('.work-ex').show();
         }
-        else {
-            $('.page-header.b-header').removeClass('slideClosed');
-        }
-
+        // if (scroll >= 400){
+        //     $('.work-ex').show();
+        //     $('.work-ex').addClass('animated fadeInUp');
+        // }
     });
 
     function getCurrentScroll() {
